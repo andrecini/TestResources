9aba5ea77f5ae832c1858abc2b93250e48b16338
andrepresys <andre.presys@gmail.com>
2021-03-05 14:47:25 -0300

    Finalized

---
+++ Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripSplitButton1 = New System.Windows.Forms.ToolStripSplitButton()
+        Me.PORTUGUÊSToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.INGLÊSToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ESPANHOLToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.numInput = New System.Windows.Forms.MaskedTextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.numOutput = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ToolStrip1.SuspendLayout
+        Me.SuspendLayout
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Name = "Label1"
+        '
+        'TextBox1
+        '
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.TextBox1.Name = "TextBox1"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripSplitButton1})
+        resources.ApplyResources(Me.ToolStrip1, "ToolStrip1")
+        Me.ToolStrip1.Name = "ToolStrip1"
+        '
+        'ToolStripSplitButton1
+        '
+        Me.ToolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripSplitButton1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PORTUGUÊSToolStripMenuItem, Me.INGLÊSToolStripMenuItem, Me.ESPANHOLToolStripMenuItem})
+        resources.ApplyResources(Me.ToolStripSplitButton1, "ToolStripSplitButton1")
+        Me.ToolStripSplitButton1.Name = "ToolStripSplitButton1"
+        '
+        'PORTUGUÊSToolStripMenuItem
+        '
+        Me.PORTUGUÊSToolStripMenuItem.Name = "PORTUGUÊSToolStripMenuItem"
+        resources.ApplyResources(Me.PORTUGUÊSToolStripMenuItem, "PORTUGUÊSToolStripMenuItem")
+        '
+        'INGLÊSToolStripMenuItem
+        '
+        Me.INGLÊSToolStripMenuItem.Name = "INGLÊSToolStripMenuItem"
+        resources.ApplyResources(Me.INGLÊSToolStripMenuItem, "INGLÊSToolStripMenuItem")
+        '
+        'ESPANHOLToolStripMenuItem
+        '
+        Me.ESPANHOLToolStripMenuItem.Name = "ESPANHOLToolStripMenuItem"
+        resources.ApplyResources(Me.ESPANHOLToolStripMenuItem, "ESPANHOLToolStripMenuItem")
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = true
+        '
+        'Button2
+        '
+        resources.ApplyResources(Me.Button2, "Button2")
+        Me.Button2.Name = "Button2"
+        Me.Button2.UseVisualStyleBackColor = true
+        '
+        'Button3
+        '
+        resources.ApplyResources(Me.Button3, "Button3")
+        Me.Button3.Name = "Button3"
+        Me.Button3.UseVisualStyleBackColor = true
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'numInput
+        '
+        resources.ApplyResources(Me.numInput, "numInput")
+        Me.numInput.Name = "numInput"
+        '
+        'Button4
+        '
+        resources.ApplyResources(Me.Button4, "Button4")
+        Me.Button4.Name = "Button4"
+        Me.Button4.UseVisualStyleBackColor = true
+        '
+        'numOutput
+        '
+        resources.ApplyResources(Me.numOutput, "numOutput")
+        Me.numOutput.Name = "numOutput"
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Name = "Label5"
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.numOutput)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.numInput)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.ToolStrip1.ResumeLayout(false)
+        Me.ToolStrip1.PerformLayout
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripSplitButton1 As ToolStripSplitButton
+    Friend WithEvents PORTUGUÊSToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents INGLÊSToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents numInput As MaskedTextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents numOutput As Label
+    Friend WithEvents ESPANHOLToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,105 @@
+﻿Imports System.Globalization
+Imports System.Threading
+
+Public Class Form1
+    Private Shared Dim current As CultureInfo = Thread.CurrentThread.CurrentUICulture
+    Private Shared Dim currentLanguage As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CreateResouceManager("Bt1")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        CreateResouceManager("Bt2")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        CreateResouceManager("Bt3")
+    End Sub
+
+    Shared Sub Main()
+        ChangeCulture("pt-BR")
+
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Form1)  'Specify the startup form
+    End Sub
+
+    Private Sub PORTUGUÊSToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PORTUGUÊSToolStripMenuItem.Click
+        ChangeCulture("")
+        ReloadForms()
+        Me.Hide()
+    End Sub
+
+    Private Sub INGLÊSToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles INGLÊSToolStripMenuItem.Click
+        ChangeCulture("en-US")
+        ReloadForms()
+        Me.Hide()
+    End Sub
+    
+    Private Sub ESPANHOLToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ESPANHOLToolStripMenuItem.Click
+        ChangeCulture("es-ES")
+        ReloadForms()
+        Me.Hide()
+    End Sub
+
+    Private Shared Sub ChangeCulture(ByVal language As String)
+        If current.TwoLetterISOLanguageName <> language
+            Dim newCulture As CultureInfo = CultureInfo.CreateSpecificCulture(language)
+            Thread.CurrentThread.CurrentUICulture = newCulture
+            ' Make current UI culture consistent with current culture.
+            Thread.CurrentThread.CurrentCulture = newCulture
+            currentLanguage = language
+        End If
+    End Sub
+
+    Private Shared Sub ReloadForms()
+        Dim form As New Form1()
+        form.Show()
+    End Sub
+
+    Private Shared Sub CreateResouceManager(ByVal message As String)
+            Dim ResMan As New Resources.ResourceManager("TesteRecursos.Messages", System.Reflection.Assembly.GetExecutingAssembly())
+            MessageBox.Show(ResMan.GetString(message))
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label2.Text = DateTime.Now().ToString()
+        SetColor()
+        SetMainFlag()
+        SetSubFlags()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        numOutput.Text = numInput.Text
+    End Sub
+
+    Private Sub SetColor()
+        If currentLanguage = "pt-BR"
+            TextBox1.ForeColor = Color.Green
+        ElseIf currentLanguage = "en-US"
+            TextBox1.ForeColor = Color.Blue
+        ElseIf currentLanguage = "es-ES"
+            TextBox1.ForeColor = Color.Orange
+        End If
+    End Sub
+
+    Private Sub SetMainFlag()
+        If currentLanguage = "pt-BR"
+            ToolStripSplitButton1.Image = Image.FromFile("C:\Users\admin\source\repos\TesteRecursos\Resources\Img\Brazil_BR_BRA_076_Flag1_26109.png")
+        ElseIf currentLanguage = "en-US"
+            ToolStripSplitButton1.Image = Image.FromFile("C:\Users\admin\source\repos\TesteRecursos\Resources\Img\UnitedStates_US_USA_840_Flag1_26093.png")
+        ElseIf currentLanguage = "es-ES"
+            ToolStripSplitButton1.Image = Image.FromFile("C:\Users\admin\source\repos\TesteRecursos\Resources\Img\Spain_flags_flag_9119.png")
+        End If
+    End Sub
+
+    Private Sub SetSubFlags()
+        PORTUGUÊSToolStripMenuItem.Image = Image.FromFile("C:\Users\admin\source\repos\TesteRecursos\Resources\Img\Brazil_BR_BRA_076_Flag1_26109.png")
+        INGLÊSToolStripMenuItem.Image = Image.FromFile("C:\Users\admin\source\repos\TesteRecursos\Resources\Img\UnitedStates_US_USA_840_Flag1_26093.png")
+        ESPANHOLToolStripMenuItem.Image = Image.FromFile("C:\Users\admin\source\repos\TesteRecursos\Resources\Img\Spain_flags_flag_9119.png")
+
+    End Sub
+
+  
+End Class
